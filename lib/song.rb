d0590f2fa4a883cd4b630ac7cd8cf4efbdb9f677
Eli Huebner <elimhuebner@gmail.com>
2020-09-15 18:25:52 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    if !(@@genres.include?(genre))
+      @@genres << genre
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres
+  end
+  
+end
